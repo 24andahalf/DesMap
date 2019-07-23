db698aa2ea87ace057d03ba163d98f72ace2d9d1
24andahalf <dave.rigley@gmail.com>
2019-07-23 13:04:28 -0300

    Add files via upload
    Initial version of the DesMap

---
+++ scripts/js/POI.js
@@ -0,0 +1,58 @@
+var POIobjs = [
+			{
+				POIid: 				"A000",
+				hoverText: 			"Hidden Undercity tunnel",
+				popupText: 			"Start point", 
+				popupHeaderImage:	"open-book",
+				x:					128,
+				y:					766
+
+			}, 
+			{
+				POIid: 				"A001",		
+				hoverText: 			"Interview room w/ secret trapdoor",
+				popupText: 			"The party entered from the trapdoor finding themselves in an interview/interrogation room. After some discussion and some snooping they moved on to the next room over, their only option aside from leaving.",
+				popupHeaderImage:	"crossed-swords",
+				x:					259,
+				y:					766
+			}, 
+			{
+				POIid: 				"A002",		
+				hoverText: 			"Interview room #2",
+				popupText: 			"Upon snooping in this room the party discovered robes of a high ranking individual at this prison, as well as master key. Vierka, one of the stealthiest of the team, volunteers to explore outside the next door which leads out into a hallway.",
+				popupHeaderImage:	"crossed-swords",
+				x:					400,
+				y:					766
+			}, 
+			{
+				POIid: 				"A003",		
+				hoverText: 			"Guarded hallway",
+				popupText: 			"Four guards patrol this area as part of a short, circular, even spaced patrol. Finding themselves without a silence spell the party attempts to take down guards one by one with the help of an invisible Vierka acting as the party's eyes and ears outside the interview room. The party takes down two guards before it is obviou their efforts have caught the other guards' attention. Upon finsihing off the 3rd guard they find the 4th has gathered others in a <button class='gotoAndSee' data-gotoID='B001'>nearby area</button and are setting up barricades.",
+				popupHeaderImage:	"crossed-swords",
+				x:					411,
+				y:					880
+			}, 
+			{
+				POIid: 				"A004",		
+				hoverText: 			"Mage holding area",
+				popupText: 			"Curiosity getting the best of them, the party agrees to explore the Mage Holding facility before facing off against the guards nearby. Maxi ventures inside to find a familiar face, insert name here :D. Through some trickery they get information on one of the team's goals at the prison, rescuing a 'John Doe'. Having realised he has been tricked the Lord of something something furious slams the bars of his cage demanding to be freed.",
+				popupHeaderImage:	"letter-bomb",
+				x:					850,
+				y:					825
+			},
+			{
+				POIid: 				"A005",
+				hoverText: 			'"Maintence Closet"',
+				popupText: 			"Seeing an opportunity to collect new trinkets and gadgets(or scrap) Stark begins nosing around inside of a maintenance closet only to find cleaning implements. After a bit more snooping he discovers the back wall is actually an illusion disguising a secret door. Whether through sheer luck, skill or some combination of the two he unlocks the door through touch alone and guides the team into a secret passage.", 
+				popupHeaderImage:	"mimic-chest",
+				x:					630,
+				y:					775
+			},
+	
+		// Add any new POI Objects in here please :)
+	
+	
+
+	
+	
+		];
---
+++ scripts/js/ntPOI.js
@@ -0,0 +1,42 @@
+var ntPOIobjs = [
+			{
+				ntPOIid: 			"B000",
+				hoverText: 			"Room that the secret door leads to",
+				popupText: 			"Contents remain unknown.", 
+				popupHeaderImage:	"open-book",
+				x:					480,
+				y:					600
+
+			}, 
+			{
+				ntPOIid: 			"B001",		
+				hoverText: 			"Guards hunkered down here",
+				popupText: 			"During the stealthy attempt to remove the threat of the guards some were alerted and have set up defenses in this room in an attempt to slow the party down.",
+				popupHeaderImage:	"crossed-swords",
+				x:					815,
+				y:					1120
+			}, 
+			{
+				ntPOIid: 			"B002",		
+				hoverText: 			"Prison Yard",
+				popupText: 			"It is likely Tezzeret may be located in this area. Dovin Baan has been seen in this area as well from time to time.",
+				popupHeaderImage:	"crossed-swords",
+				x:					1550,
+				y:					315
+			}
+			
+	
+	
+		// Add any new POI Objects in here please :)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		];
